5878657b3d057a9f746c9181d51a765810259a80
CarolineSMachado <caroline.machadobz@gmail.com>
2024-07-22 19:13:50 -0300

    API Rest com Gradle

---
+++ projeto/src/main/java/dio/projeto/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author Pichau
+ *
+ */
+module projeto {
+	requires jdk.jpackage;
+	requires java.desktop;
+	requires java.xml;
+	requires jdk.compiler;
+	requires jdk.javadoc;
+}
---
+++ projeto/src/main/java/dio/projeto/src/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package service.impl;
+
+import org.springframework.stereotype.Service;
+
+import model.User;
+import repository.UserRepository;
+import service.UserService;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+
+	@Override
+	public service.User create(service.User userToCreate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
